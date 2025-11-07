1d61ecfc67dc3a86be46c02b5b36f2f77410d086
Hermes Mavinga <hermesmavinga2019@gmail.com>
2025-11-07 23:30:43 +0100

    ajout des log pour debug

--- app/controllers/session_controller.ts
+++ app/controllers/session_controller.ts
@@ -3,11 +3,17 @@ import User from '#models/user'
 
 export default class SessionController {
   public async loginStore({ request, auth, response }: HttpContext) {
-    const { email, password } = request.only(['email', 'password'])
+    try {
+      const { email, password } = request.only(['email', 'password'])
 
-    const user = await User.verifyCredentials(email, password)
+      const user = await User.verifyCredentials(email, password)
+      console.log(user)
 
-    await auth.use('web').login(user)
-    response.redirect('/dashboard')
+      await auth.use('web').login(user)
+      response.redirect('/dashboard')
+    } catch (error) {
+      console.error('utilisateur non trouve')
+      console.log('utilisateur non trouve')
+    }
   }
 }
